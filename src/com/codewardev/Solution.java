6e8f82781fff7877f7a0c0a79657b541f372b959
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-12 16:08:05 +1100

    first working cut

--- src/com/codewardev/Solution.java
+++ src/com/codewardev/Solution.java
@@ -1,10 +1,16 @@
 package com.codewardev;
 
+// https://www.codewars.com/kata/5976575438480829ff000041/train/java
+
 public class Solution {
 
 	public static String CalculateReward(int currentPoint, int numJewelry) {
+		int totalPoints = currentPoint + numJewelry;
+		int pointLeft = totalPoints % 6;
+		int freeItems = totalPoints / 6;
+		int payItems = numJewelry - freeItems;
 		
-		return "";
+		return "Point Left: "+pointLeft+". Free Item: "+freeItems+". Pay Item: "+payItems;
 	}
 
 }
